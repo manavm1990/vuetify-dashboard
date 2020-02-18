a06fa538fd58cd97ade76087aa16119149ada0c4
Manav Misra <manavm@visionwebsoft.com>
2020-02-17 21:36:09 -0600

    :sparkles: Add login page as 'content.'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,40 @@
 <template>
   <v-app>
     <v-card><h1 class="display-1">Fat Stuff!</h1> </v-card>
+    <v-content>
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Fat Stuff!</h1>
+        </v-card-title>
+
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              name="username"
+              label="Username"
+              id="username"
+              prepend-icon="mdi-account-circle"
+            ></v-text-field>
+            <v-text-field
+              name="password"
+              label="Password"
+              id="password"
+              :type="showPassword ? 'text' : 'password'"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+
   </v-app>
 </template>
 
