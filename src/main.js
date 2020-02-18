08361d70c14e3287c298a71740e4e84e85ab38bd
Manav Misra <manavm@visionwebsoft.com>
2020-02-18 11:56:07 -0600

    :heavy_plus_sign: Add nProgress. :children_crossing: Add nprogress bar when loading dashboard.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 
+import "nprogress/nprogress.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
