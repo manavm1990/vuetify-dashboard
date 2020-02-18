07012034ba795d252db17f7686bd3d7e1d4e477d
Manav Misra <manavm@visionwebsoft.com>
2020-02-18 08:01:51 -0600

    :sparkles: Move Login to its own view.
    :sparkles: Use router to route to Login view.

--- src/App.vue
+++ src/App.vue
@@ -10,37 +10,7 @@
     </v-app-bar>
 
     <v-content>
-      <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Fat Stuff!</h1>
-        </v-card-title>
-
-        <v-card-text>
-          <v-form>
-            <v-text-field
-              name="username"
-              label="Username"
-              id="username"
-              prepend-icon="mdi-account-circle"
-            ></v-text-field>
-            <v-text-field
-              name="password"
-              label="Password"
-              id="password"
-              :type="showPassword ? 'text' : 'password'"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            ></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-content>
 
     <v-footer color="primary lighten-1" padless>
